d4afe3f54f8e9a74af55c927c860c62dff1b7d81
zxj <625150103@qq.com>
2021-09-01 22:57:31 +0800

    animation增加切换动画

--- animation/src/main/java/com/zxj/animation/MainActivity.kt
+++ animation/src/main/java/com/zxj/animation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.zxj.animation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.zxj.animation.fragment.FirstFragment
 import com.zxj.animation.fragment.FourthFragment
@@ -9,31 +10,38 @@ import com.zxj.animation.fragment.ThirdFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val index = 3
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<View>(R.id.btn_next).setOnClickListener {
+            next()
+            index++
+        }
+    }
+
+    private fun next() {
         val bt = supportFragmentManager.beginTransaction()
-        when (index) {
+        when (index % 4) {
             /* 题目一：属性动画 */
-            1 -> {
+            0 -> {
                 bt.replace(R.id.fl_content, FirstFragment.newInstance())
             }
 
             /* 题目二：自定义估值动画[PointF] */
-            2 -> {
+            1 -> {
                 bt.replace(R.id.fl_content, SecondFragment.newInstance())
             }
 
             /* 题目三：多属性动画 */
-            3 -> {
+            2 -> {
                 bt.replace(R.id.fl_content, ThirdFragment.newInstance())
             }
 
             /* 题目三：自定义估值动画[String] */
-            4 -> {
+            3 -> {
                 bt.replace(R.id.fl_content, FourthFragment.newInstance())
             }
         }
