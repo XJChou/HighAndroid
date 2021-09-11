0fbceed4ed1356eb9036c560807e85838dc6ffd2
zxj <625150103@qq.com>
2021-09-11 17:19:14 +0800

    cutsomLayout module 创建

---
+++ customlayout/src/main/java/com/zxj/customlayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zxj.customlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
