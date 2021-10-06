5e7267d11737987d1b237d511ddc997b1997b3ce
zxj <625150103@qq.com>
2021-10-06 18:50:23 +0800

    ScalableImageView增加移动[15%]

--- touch/src/main/java/com/zxj/touch/scalable/ScalableImageView.kt
+++ touch/src/main/java/com/zxj/touch/scalable/ScalableImageView.kt
@@ -16,9 +16,10 @@ import com.zxj.touch.R
 
 /**
  * 1、撑满宽或者高      [Finish]
- * 2、双击指定地方放大   [双击完成]
+ * 2、双击指定地方放大   [双击完成 + 放大完成 + ]
  * 3、手指交互放大和缩小 []
- * 4、当图片过大可以缩放
+ * 4、图片可以拖拽 [移动完成]
+ * 5、边界处理
  */
 class ScalableImageView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
@@ -26,8 +27,27 @@ class ScalableImageView(context: Context, attrs: AttributeSet) : View(context, a
 
     private lateinit var bitmap: Bitmap
 
+    private var originX = 0f
+    private var originY = 0f
+
     private var offsetX = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
     private var offsetY = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var scale = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var minScale = 1f
+    private var maxScale = 2f
 
     /* 手势 */
     private val mGestureDetector = GestureDetectorCompat(context, ScalableGestureDetector())
@@ -42,36 +62,37 @@ class ScalableImageView(context: Context, attrs: AttributeSet) : View(context, a
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
-        /* 计算图片比例 */
+        /* 1、计算图片比例 */
         val options = BitmapFactory.Options().also {
             it.inJustDecodeBounds = true
         }
         BitmapFactory.decodeResource(resources, R.drawable.rengwuxian, options)
 
-        /* 设置图片撑满一边 */
+        /* 2、计算最小缩放比，最大缩放比 */
         val ratio = options.outWidth.toFloat() / options.outHeight
         val viewRatio = width.toFloat() / height
         if (ratio >= viewRatio) {
-            /* 宽撑满 */
-            offsetX = 0f
-            offsetY = (height - width / ratio) / 2f
-
-            bitmap = resources.decodeResource(R.drawable.rengwuxian, width)
+            minScale = width / options.outWidth.toFloat()
+            maxScale = height / options.outHeight.toFloat()
         } else {
-            /* 高撑满 */
-            offsetX = (width - ratio * height) / 2f
-            offsetY = 0f
-
-            bitmap = resources.decodeResource(R.drawable.rengwuxian, ratio * height)
+            minScale = height / options.outHeight.toFloat()
+            maxScale = width / options.outWidth.toFloat()
         }
+
+        scale = minScale
+        bitmap = resources.decodeResource(R.drawable.rengwuxian, options.outWidth)
+
+        originX = (width - options.outWidth) / 2f
+        originY = (height - options.outHeight) / 2f
     }
 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        /* 2、点击指定位置方法 */
-        /* 1、绘制图片 */
-        canvas.drawBitmap(bitmap, offsetX, offsetY, paint)
+        canvas.translate(originX + bitmap.width / 2f, originY + bitmap.height / 2f)
+        canvas.scale(scale, scale)
+        canvas.translate(-originX - bitmap.width / 2f, -originY - bitmap.height / 2f)
+        canvas.drawBitmap(bitmap, originX + offsetX, originY + offsetY, paint)
     }
 
     inner class ScalableGestureDetector : GestureDetector.SimpleOnGestureListener() {
@@ -80,9 +101,35 @@ class ScalableImageView(context: Context, attrs: AttributeSet) : View(context, a
             return true
         }
 
+        override fun onScroll(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            /* 因为位移会放大scale倍，所以这里要先除一下 */
+            offsetX -= distanceX / scale
+            offsetY -= distanceY / scale
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
+
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            Toast.makeText(context, "onFling", Toast.LENGTH_SHORT).show()
+            return super.onFling(e1, e2, velocityX, velocityY)
+        }
+
         override fun onDoubleTap(e: MotionEvent): Boolean {
-            Toast.makeText(context, "双击", Toast.LENGTH_SHORT).show()
-            return super.onDoubleTap(e)
+            scale = if (scale == maxScale) {
+                minScale
+            } else {
+                maxScale
+            }
+            return true
         }
     }
 }
