f8a717fab097c64b763cdbb7afbbe215dcca7ace
zxj <625150103@qq.com>
2022-02-28 23:36:47 +0800

    增加文字README.md

--- text/src/main/java/com/zxj/text/MainActivity.kt
+++ text/src/main/java/com/zxj/text/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.zxj.text
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.zxj.text.view.RingView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ringView: RingView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ringView = findViewById(R.id.ringview)
+    }
+
+    fun switchMode(view: View) {
+        ringView.switchMode()
     }
 }
